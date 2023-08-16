7f47d273c0184df2325b7561c54b03c97676d22c
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-08-16 13:53:48 -0700

    working on plug-in

---
+++ Controllers/DesignAutomationController.cs
@@ -0,0 +1,76 @@
+﻿using Autodesk.Forge;
+using Autodesk.Forge.Client;
+using Autodesk.Forge.DesignAutomation;
+using Autodesk.Forge.DesignAutomation.Model;
+using Autodesk.Forge.Model;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Activity = Autodesk.Forge.DesignAutomation.Model.Activity;
+using Alias = Autodesk.Forge.DesignAutomation.Model.Alias;
+using AppBundle = Autodesk.Forge.DesignAutomation.Model.AppBundle;
+using Parameter = Autodesk.Forge.DesignAutomation.Model.Parameter;
+using WorkItem = Autodesk.Forge.DesignAutomation.Model.WorkItem;
+using WorkItemStatus = Autodesk.Forge.DesignAutomation.Model.WorkItemStatus;
+
+
+namespace designAutomationSample.Controllers
+{
+    [ApiController]
+    public class DesignAutomationController : ControllerBase
+    {
+        // Used to access the application folder (temp location for files & bundles)
+        private IWebHostEnvironment _env;
+        // used to access the SignalR Hub
+        private IHubContext<DesignAutomationHub> _hubContext;
+        // used to store the s3 upload payload;
+        private static PostCompleteS3UploadPayload _postCompleteS3UploadPayload;
+        // Local folder for bundles
+        public string LocalBundlesFolder { get { return Path.Combine(_env.WebRootPath, "bundles"); } }
+        /// Prefix for AppBundles and Activities
+        public static string NickName { get { return OAuthController.GetAppSetting("APS_CLIENT_ID"); } }
+        /// Alias for the app (e.g. DEV, STG, PROD). This value may come from an environment variable
+        public static string Alias { get { return "dev"; } }
+        //This property manager S3 Upload Payload
+        public static PostCompleteS3UploadPayload S3UploadPayload
+        {
+            get { return _postCompleteS3UploadPayload; }
+            set { _postCompleteS3UploadPayload = value; }
+        }
+        // Design Automation v3 API
+        DesignAutomationClient _designAutomation;
+
+        // Constructor, where env and hubContext are specified
+        public DesignAutomationController(IWebHostEnvironment env, IHubContext<DesignAutomationHub> hubContext, DesignAutomationClient api)
+        {
+            _designAutomation = api;
+            _env = env;
+            _hubContext = hubContext;
+        }
+
+        // **********************************
+        //
+        // Next we will add the methods here
+        //
+        // **********************************
+    }
+
+    /// <summary>
+    /// Class uses for SignalR
+    /// </summary>
+    public class DesignAutomationHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        public string GetConnectionId() { return Context.ConnectionId; }
+    }
+
+}
